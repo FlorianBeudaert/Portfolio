fedd1f6fe0ae8f41abbb34df2c3b545783237004
Florian <florian.beudaert@epitech.eu>
2024-06-25 10:42:21 +0200

    home page and daisy ui

---
+++ src/components/about_me.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutMe = () => {
+    return (
+        <div className="grid grid-cols-2 gap-4 h-screen pt-16" id="aboutme">
+        </div>
+    );
+}
+
+export default AboutMe
---
+++ src/components/navbar.js
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-scroll';
+
+const Navbar = () => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+
+    return (
+        <div>
+            <nav className="navbar bg-base-200 fixed w-full z-[1]">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex="0" role="button" className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-primary"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h7"/>
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex="0"
+                            className="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <Link to="home" smooth={true} duration={500}>Accueil</Link>
+                            </li>
+                            <li>
+                                <Link to="aboutme" smooth={true} duration={500}>A propos</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <p className="navbar-brand text-primary cursor-default">Portfolio | Florian Beudaert</p>
+                </div>
+                <div className="navbar-end">
+                    <label className="flex cursor-pointer gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 text-primary"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <circle cx="12" cy="12" r="5"/>
+                            <path
+                                d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>
+                        </svg>
+                        <input type="checkbox" value="synthwave" className="toggle border border-primary bg-primary hover:bg-primary" onClick={toggleTheme}/>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 text-primary"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                        </svg>
+                    </label>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
